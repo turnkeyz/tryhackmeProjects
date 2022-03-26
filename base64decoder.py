a293e8e0b70c0c7ce9f405c9ccd7cce84418fd52
turnkeyz <kylertelge@gmail.com>
2022-03-25 23:44:27 -0400

    progress on decoding

---
+++ base64decoder.py
@@ -0,0 +1,19 @@
+import base64
+
+def encoder():
+    message = "encoding is python is easy!"
+    msg_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(msg_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    print('\n',base64_message)
+
+
+def decoder():
+    base64_message = 'ZW5jb2RpbmcgaXMgcHl0aG9uIGlzIGVhc3kh'  
+    base64_bytes = base64_message.encode('ascii')
+    msg_bytes = base64.b64decode(base64_bytes)
+    decoded_message = msg_bytes.decode('ascii')
+    print('\n', decoded_message)
+
+if __name__ == '__main__':
+    decoder()
