22385e63e7be2c0eb7183bee232f272dac951925
turnkeyz <kylertelge@gmail.com>
2022-03-26 00:11:38 -0400

    finished decoder and got the flag!

--- base64decoder.py
+++ base64decoder.py
@@ -1,19 +1,29 @@
 import base64
 
 def encoder():
-    message = "encoding is python is easy!"
+    message = "stuff"
     msg_bytes = message.encode('ascii')
     base64_bytes = base64.b64encode(msg_bytes)
     base64_message = base64_bytes.decode('ascii')
     print('\n',base64_message)
 
 
-def decoder():
-    base64_message = 'ZW5jb2RpbmcgaXMgcHl0aG9uIGlzIGVhc3kh'  
-    base64_bytes = base64_message.encode('ascii')
-    msg_bytes = base64.b64decode(base64_bytes)
-    decoded_message = msg_bytes.decode('ascii')
-    print('\n', decoded_message)
+def decoder(amount):
+    file = open('b64.txt', 'r').read()
+    #print(file,'\n')
+    #stuff = 'c3R1ZmY='
+    i = 0
+    for i in range(amount):
+        print(i)
+        file = base64.b64decode(file)
+    print(file)
+#   base64_message = 'ZW5jb2RpbmcgaXMgcHl0aG9uIGlzIGVhc3kh'  
+#   base64_bytes = base64_message.encode('ascii')
+#   msg_bytes = base64.b64decode(base64_bytes)
+#   decoded_message = msg_bytes.decode('ascii')
+#   print('\n', decoded_message)
+
+
 
 if __name__ == '__main__':
-    decoder()
+    decoder(int(input()))
